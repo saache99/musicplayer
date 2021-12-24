90e4e680003de777f8497bf68e2a7628f39e6a29
saache99 <91719995+saache99@users.noreply.github.com>
2021-12-24 17:53:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+leftX = 0;
+rightX = 0;
+leftY = 0;
+rightY = 0;
+
 function setup()
 {
     canvas = createCanvas(600,900);
@@ -5,11 +10,33 @@ function setup()
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video,modelme);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelme()
+{
+    console.log("Model Is loaded");
 }
 
 function draw()
 {
     image(video,0,0,600,500);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftX = results[0].pose.leftWrist.x;
+        leftY = results[0].pose.leftWrist.y;
+        rightX = results[0].pose.rightWrist.x;
+        rightY = results[0].pose.rightWrist.y;
+
+        console.log("Values got by your hands are:-"+leftX+leftY+rightX+rightY);
+    }
 }
 
 function preload()
